ee5f92bd763868a48073fc048a87a8a174a96c73
Andinho83 <Andinho83@users.noreply.github.com>
2018-07-11 16:18:46 +0200

    dynamic page load with exception

--- ContentManager.php
+++ ContentManager.php
@@ -1,29 +1,47 @@
 <?php 
 
-$files = array();
-if( $handle = opendir("inc") ) {
-	while( $file = readdir($handle) ) {
-		if( strstr($file, '.php') ) {
-			array_push($files, $file);
+/**
+ * \brief getWhitelist
+ *
+ * Returns a indexed whitelist for dynamic page load.
+ *
+ * \param	Relative path from DOCUMENT_ROOT
+ * \param	File type of the indexed pages
+ * \return	Indexed whitelist
+ *
+ */
+function getWhitelist($relPath, $filetype) {
+	if( $handle = opendir($relPath) ) {
+		while( $file = readdir($handle) ) {
+			if( strstr($file, '.'.$filetype) ) {
+				$index = substr($file, 0, strpos($file, '.', 0));
+				$whitelist[$index] = $relPath."/".$file; 
+			}
 		}
+		closedir($handle);
+		return $whitelist;
+	}
+	else {
+		throw new Exception("Relative path \"".$relPath."\" is wrong or does not exist!");
 	}
 }
 
-var_dump($files);
-
-$whitelist = array(	'login' => 'inc/login.php', 
-					'register' => 'inc/register.php', 
-					'home' => 'inc/home.php');
-
-if( empty($_REQUEST) ) {	
-	$_REQUEST['id'] = 'home';
-}
+try {
+	$whitelist = getWhitelist("inc", "php");
 	
-if( array_key_exists($_REQUEST['id'], $whitelist) ) {
-	include_once($whitelist[$_REQUEST['id']]);
+	if( empty($_REQUEST) ) {	
+		$_REQUEST['id'] = 'home';
+	}
+		
+	if( array_key_exists($_REQUEST['id'], $whitelist) ) {
+		include_once($whitelist[$_REQUEST['id']]);
+	}
+	else {
+		include_once($whitelist['home']);
+	}
 }
-else {
-	include_once($_REQUEST['id']);
+catch( Exception $e ) {
+	echo "<p class=\"error\"><b>Exception: ".$e->getMessage()."</b></p>";
 }
 
 ?>
